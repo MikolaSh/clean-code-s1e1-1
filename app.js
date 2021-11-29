bde0fa22dc2ce0aa426fe64d90a3b185c624a67c
MikolaSh <nikolashulga2000@gmail.com>
2021-11-29 23:23:47 +0300

    refactor: add class name to img tags according to rule 3.4 in index.html, style.css, app.js

--- app.js
+++ app.js
@@ -32,6 +32,7 @@ var createNewTaskElement=function(taskString){
     var deleteButton=document.createElement("button");//delete button
 
     var deleteButtonImg=document.createElement("img");//delete button image
+    deleteButtonImg.classList.add("remove-img")
 
     label.innerText=taskString;
     label.className='task';
